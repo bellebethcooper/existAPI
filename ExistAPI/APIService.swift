7ab854a92638befef41fc04818399e95cfdb6c78
Belle Beth Cooper <bellebethcooper@gmail.com>
2018-11-10 10:08:53 +1100

    Started adding files

---
+++ ExistAPI/APIService.swift
@@ -0,0 +1,14 @@
+//
+//  APIService.swift
+//  ExistAPI
+//
+//  Created by Belle Beth Cooper on 10/11/18.
+//  Copyright © 2018 Hello Code. All rights reserved.
+//
+
+import Foundation
+
+
+class ExistAPI {
+    
+}
